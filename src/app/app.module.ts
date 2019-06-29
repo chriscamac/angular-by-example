53223829c35ea79626748c36702565194a7afd21
Chris Camac <ccamac07@gmail.com>
2019-06-29 14:02:05 -0400

    add ui module and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, UiModule],
     providers: [],
     bootstrap: [AppComponent],
 })
