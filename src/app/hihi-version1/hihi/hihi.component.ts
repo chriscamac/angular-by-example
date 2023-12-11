fb0d905624f5ccd2227cbf7fe7dfa7b54fd13d7d
twgwuy <tothituongvyfornothing@gmail.com>
2023-12-11 23:15:03 +0700

    Add demo

---
+++ src/app/hihi-version1/hihi/hihi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hihi',
+  templateUrl: './hihi.component.html',
+  styleUrls: ['./hihi.component.less']
+})
+export class HihiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
